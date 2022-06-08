9067536ed47bb48283bc6c40237328362354eb68
Rhett L <webster117@hotmail.com>
2022-06-08 09:49:34 -0600

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Rhett Levitz on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
